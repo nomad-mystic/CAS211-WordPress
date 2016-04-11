64a4fb8b7325d908d05db2949eb8441a792b834f
nomad-mystic <nomadmystics@gmail.com>
2016-04-11 02:03:13 -0700

    Created Sage.io starter theme setup, connected to remote server , connected to local SQL database, and exported SQL tables from remote to local

---
+++ offline/themes/twentyfouteen-child/footer.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?>
+
+		</div><!-- #main -->
+
+<!--footer begins-->
+<div class="color-border">
+</div>
+<div class="footer">
+	<div class="container">
+		<div class="infooter">
+			<p class="copyright">Copyright &copy; Nifty Tours 2016</p>
+		</div>
+		<ul class="socialmedia">
+			<li><a href="https://www.facebook.com/NiftyTours" target="_blank">
+					<i class="icon-facebook"></i>
+				</a>
+			</li>
+			<li><a href=""><i class="icon-twitter"></i></a></li>
+			<li><a href=""><i class="icon-dribbble"></i></a></li>
+			<li><a href=""><i class="icon-linkedin"></i></a></li>
+			<li><a href=""><i class="icon-instagram"></i></a></li>
+		</ul>
+	</div>
+</div>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$('#simple-menu').sidr({
+			side: 'right'
+		});
+	});
+	$(document).ready(function() {
+		$('.responsive-menu-button').sidr({
+			name: 'sidr-main',
+			source: '#navigation',
+			side: 'right'
+
+		});
+	});
+	$(document).ready(
+		function() {
+			$("html").niceScroll({cursorborder:"0px solid #fff",cursorwidth:"5px",scrollspeed:"70"});
+		}
+	);
+</script>
+</body>
+</html>
---
+++ offline/themes/twentyfouteen-child/header.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * The Header for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) & !(IE 8)]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
+	<![endif]-->
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+	<div class="header">
+		<div class="container">
+			<div class="logo-menu">
+				<div class="logo">
+					<p><a href="#"><img src="http://nomadmystic.com/CAS211W/nifty/images/nifty-tours-logo.png" alt="Nifty Tours Logo"/></a></p>
+				</div>
+				<!--<a id="simple-menu" href="#sidr">Toggle menu</a>-->
+				<div id="mobile-header">
+					<a class="responsive-menu-button" href="#"><img src="http://nomadmystic.com/CAS211W/nifty/images/11.png"/></a>
+				</div>
+				<div class="menu" id="navigation">
+					<ul>
+						<li><a href="http://nomadmystic.com/CAS211W/nifty/">Home</a></li>
+						<li><a href="http://nomadmystic.com/CAS211W/nifty/#features">Services</a></li>
+						<li><a href="#">Stories</a></li>
+						<li><a href="http://nomadmystic.com/CAS211W/nifty/">Shop</a></li>
+						<li><a href="http://nomadmystic.com/CAS211W/nifty/#contact">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+    <div class="color-border"></div>
+	<div class="clearit"></div>
+	<!--header ends-->
